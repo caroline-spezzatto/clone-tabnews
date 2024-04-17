e004ed06ac000c509b0e8346acde30c895bf78e9
Caroline Spezzatto <caroline.spezzatto@paketa.com.br>
2024-04-16 21:18:30 -0300

    fix status page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,22 +1,33 @@
 import database from 'infra/database.js'
-
 async function status(request, response) {
   const updatedAt = new Date().toISOString()
-  const postgresVersion = await database.query({
-    text: 'SELECT version()'
+
+  const databaseVersionResul = await database.query('SHOW server_version;')
+  const databaseVersionValue = databaseVersionResul.rows[0].server_version
+
+  const databaseMaxConnectionsResult = await database.query(
+    'SHOW max_connections;'
+  )
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenedConnectionsResult = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName]
   })
-  const maxConnections = await database.query({
-    text: 'SHOW max_connections'
-  })
-  const usedConnections = await database.query({
-    text: 'SELECT count(*) FROM pg_stat_activity'
-  })
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count
 
   response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: postgresVersion,
-    max_connections: maxConnections,
-    used_connections: usedConnections
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue
+      }
+    }
   })
 }
 
